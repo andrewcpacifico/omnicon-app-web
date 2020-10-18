c79b6d28cb7e9fe9adaf9d39bdfa38f78dc663fe
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2020-10-18 17:52:56 -0400

    feat: add TaskList component, with mocked data

---
+++ src/components/TaskList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-subheader>Teste</v-subheader>
+    <v-list>
+      <v-list-item v-for="task  in tasks" v-bind:key="task.id">{{ task.name }}</v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import taskService from '../services/task';
+
+export default {
+  name: 'Tasklist',
+  data: () => ({
+    tasks: taskService.getAll(),
+  }),
+};
+</script>
---
+++ src/services/task.js
@@ -0,0 +1,28 @@
+const TASKS = [
+  { id: 0, name: 'Task 00' },
+  { id: 1, name: 'Task 01' },
+  { id: 2, name: 'Task 02' },
+  { id: 3, name: 'Task 03' },
+  { id: 4, name: 'Task 04' },
+  { id: 5, name: 'Task 05' },
+  { id: 6, name: 'Task 06' },
+  { id: 7, name: 'Task 07' },
+  { id: 8, name: 'Task 08' },
+  { id: 9, name: 'Task 09' },
+  { id: 10, name: 'Task 10' },
+  { id: 11, name: 'Task 11' },
+  { id: 12, name: 'Task 12' },
+  { id: 13, name: 'Task 13' },
+  { id: 14, name: 'Task 14' },
+  { id: 15, name: 'Task 15' },
+  { id: 16, name: 'Task 16' },
+  { id: 17, name: 'Task 17' },
+  { id: 18, name: 'Task 18' },
+  { id: 19, name: 'uma task grande fudida com um baita título gigantesco apra um caralho' },
+];
+
+export default {
+  getAll() {
+    return TASKS;
+  },
+};
