e8db0a177733ddc307b84617ffe5b6fd7bc5dabd
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2020-10-20 10:03:09 -0400

    feat(services/task): consume rest api to fetch tasks

--- src/services/task.js
+++ src/services/task.js
@@ -1,30 +1,10 @@
-const TASKS = [
-  { id: 0, name: 'Task 00' },
-  { id: 1, name: 'Task 01' },
-  { id: 2, name: 'Task 02' },
-  { id: 3, name: 'Task 03' },
-  { id: 4, name: 'Task 04' },
-  { id: 5, name: 'Task 05' },
-  { id: 6, name: 'Task 06' },
-  { id: 7, name: 'Task 07' },
-  { id: 8, name: 'Task 08' },
-  { id: 9, name: 'Task 09' },
-  { id: 10, name: 'Task 10' },
-  { id: 11, name: 'Task 11' },
-  { id: 12, name: 'Task 12' },
-  { id: 13, name: 'Task 13' },
-  { id: 14, name: 'Task 14' },
-  { id: 15, name: 'Task 15' },
-  { id: 16, name: 'Task 16' },
-  { id: 17, name: 'Task 17' },
-  { id: 18, name: 'Task 18' },
-  { id: 19, name: 'uma task grande fudida com um baita título gigantesco apra um caralho' },
-];
+import axios from 'axios';
+
+import config from '../config';
 
 export default {
-  getAll() {
-    return new Promise((resolve) => {
-      setTimeout(() => { resolve(TASKS); }, 3000);
-    });
+  async getAll() {
+    const { data } = await axios.get(`${config.api.baseUrl}/v1/tasks`);
+    return data;
   },
 };
