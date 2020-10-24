17396ee4d7afae7363b1f91555a8bdc938ef7eed
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2020-10-24 12:15:34 -0400

    feat: add infinite scroll base implementation to task list

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <v-subheader>Teste</v-subheader>
-    <v-list v-if="status === 'loaded'">
-      <v-list-item v-for="task  in tasks" v-bind:key="task._id">{{ task.title }}</v-list-item>
-    </v-list>
+    <div class="list-wrapper" v-if="status === 'loaded'">
+      <v-list>
+        <v-list-item v-for="task  in tasks" v-bind:key="task._id">{{ task.title }}</v-list-item>
+      </v-list>
+
+      <v-progress-circular
+        class="scroll-loader"
+        ref="scrollLoader"
+        indeterminate
+      ></v-progress-circular>
+    </div>
 
     <v-progress-circular class="loader"
       v-else
@@ -14,18 +22,52 @@
 </template>
 
 <script>
+import debounce from 'lodash.debounce';
 import taskService from '../services/task';
+
+const SCROLL_MARGIN = 5;
 
 export default {
   name: 'Tasklist',
   data: () => ({
     tasks: [],
     status: 'loading',
+    loadingData: false,
   }),
   async beforeCreate() {
+    this.loadingData = true;
     const tasks = await taskService.getAll();
     this.$data.tasks = tasks;
     this.status = 'loaded';
+    this.loadingData = false;
+  },
+  created() {
+    this.handleDebouncedScroll = debounce(this.handleScroll, 300);
+    window.addEventListener('scroll', this.handleDebouncedScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleDebouncedScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (this.loadingData) {
+        return;
+      }
+
+      const { scrollLoader } = this.$refs;
+      const { top } = scrollLoader.$el.getBoundingClientRect();
+
+      if (top <= window.innerHeight - SCROLL_MARGIN) {
+        this.loadMoreData();
+      }
+    },
+    loadMoreData() {
+      console.log('loading data...');
+      this.loadingData = true;
+      setTimeout(() => {
+        this.loadingData = false;
+      }, 3000);
+    },
   },
 };
 </script>
@@ -36,5 +78,15 @@ export default {
     transform: translate(-50%, -50%);
     position: absolute;
     top: 50%;
+  }
+
+  .list-wrapper {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .scroll-loader {
+    margin-top: 20px;
   }
 </style>
--- src/services/task.js
+++ src/services/task.js
@@ -4,7 +4,7 @@ import config from '../config';
 
 export default {
   async getAll() {
-    const { data } = await axios.get(`${config.api.baseUrl}/v1/tasks`);
+    const { data } = await axios.get(`${config.api.baseUrl}/v1/tasks?limit=20`);
     return data;
   },
 };
